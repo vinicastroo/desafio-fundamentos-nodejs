fa91e71cd3e3a9f6fc02e5ba3cadfa31ada6bdc6
Vinicius Castro <viniciusc.d.c@hotmail.com>
2020-10-21 15:47:55 -0300

    Desafio completo!

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,23 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (!(type === 'income' || type === 'outcome')) {
+      throw Error("The type is wrong, it can be just 'income' or 'outcome'");
+    }
+
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      let totalOutcome = balance.outcome + value;
+
+      if (totalOutcome > balance.income) {
+        throw Error("the outcome cannot be greater than the income");
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({ title, value, type });
+
+    return transaction;
   }
 }
 
